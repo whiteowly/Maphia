6c085491b0463e92b923f337a5f70a5008cc32a9
whiteowly <jerryayalew696@gmail.com>
2026-01-27 16:58:55 +0300

    build ready v2

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,26 @@
 import { fontFamily } from '@/dimensions/fontFamily';
 import { useFonts } from 'expo-font';
 import { Stack } from "expo-router";
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
 import '../global.css';
 import { AlertProvider } from './context/AlertContext';
 import { GameProvider } from './context/GameContext';
 import { MusicProvider } from './context/MusicContext';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
     [fontFamily.IrishGrover]: require('../assets/fonts/IrishGrover-Regular.ttf'),
     [fontFamily.Gruesome]: require('../assets/fonts/Gruesome.ttf'),
   });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
 
   if (!fontsLoaded) {
     return null;
